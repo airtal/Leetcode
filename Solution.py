ee4c26570da24dce69f66ebb937840f4cf46153d
airtal <cyx90214@gmail.com>
2020-04-21 17:59:29 -0700

    Add isSameTree().

--- Solution.py
+++ Solution.py
@@ -314,3 +314,9 @@ class Solution:
         
         return s1[-1] == s3[-1] and self.isInterleave(s1[:-1], s2, s3[:-1]) or \
                s2[-1] == s3[-1] and self.isInterleave(s1, s2[:-1], s3[:-1])
+
+    # https://leetcode.com/problems/same-tree/
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:        
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
