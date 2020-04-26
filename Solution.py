9030b5b3445a4c1df6b05b41bd0f48d934311fce
airtal <cyx90214@gmail.com>
2020-04-26 12:08:01 -0700

    Add longestCommonSubsequence().

--- Solution.py
+++ Solution.py
@@ -484,3 +484,15 @@ class Solution:
                     ans.append([])
                 ans[i + j].append(num)
         return [x for row in ans for x in reversed(row)]
+
+    # https://leetcode.com/problems/longest-common-subsequence/
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1, n2 = len(text1), len(text2)
+        f = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+        for i in range(n1):
+            for j in range(n2):
+                if text1[i] == text2[j]:
+                    f[i + 1][j + 1] = f[i][j] + 1
+                else:
+                    f[i + 1][j + 1] = max(f[i + 1][j], f[i][j + 1])
+        return f[n1][n2]    
