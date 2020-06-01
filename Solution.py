86eb93c3c278f46175c3f586bca1c7979ba32cf7
airtal <cyx90214@gmail.com>
2020-06-01 14:23:16 -0700

    Add invertTree().

--- Solution.py
+++ Solution.py
@@ -1207,3 +1207,18 @@ class StockSpanner:
                 else:
                     f[i + 1][j + 1] = min(f[i][j], f[i][j + 1], f[i + 1][j]) + 1
         return f[m][n]
+
+    # https://leetcode.com/problems/invert-binary-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root: return root
+        temp = self.invertTree(root.left)
+        root.left = self.invertTree(root.right)
+        root.right = temp
+        return root
