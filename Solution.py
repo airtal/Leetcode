e1186bec5e3730ec28aef4bad1727769842196c6
airtal <cyx90214@gmail.com>
2020-07-08 15:21:53 -0700

    Add threeSum().

--- Solution.py
+++ Solution.py
@@ -1623,3 +1623,22 @@ class RandomizedSet:
                     if j >= 1 and grid[i][j - 1]: left += 1
                     if i >= 1 and grid[i - 1][j]: up += 1
         return cnt * 4 - left * 2 - up * 2
+
+    # https://leetcode.com/problems/3sum/
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res, n = [], len(nums)
+        for i in range(n - 2):
+            if nums[i] > 0: break
+            if i and nums[i] == nums[i - 1]: continue
+            j, k = i + 1, n - 1
+            while j < k:
+                if nums[j] + nums[k] == -nums[i]:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]: j += 1
+                    while j < k and nums[k] == nums[k + 1]: k -= 1
+                elif nums[j] + nums[k] < -nums[i]: j += 1
+                else: k -= 1
+        return res
